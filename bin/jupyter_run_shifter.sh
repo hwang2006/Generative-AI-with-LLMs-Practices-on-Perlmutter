ac1a5bc67b30c6429238db67dcf58ff1149aaa3a
Soonwook Hwang <hwang@kisti.re.kr>
2023-11-05 01:35:24 -0700

    Initial commit

---
+++ bin/jupyter_run_shifter.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --image=qualis2006/genai-pytorch-22.09-py3:latest
+#SBATCH -A dasrepo_g
+#SBATCH -C gpu
+##SBATCH -q regular
+#SBATCH -q shared
+#SBATCH -t 8:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH -c 32
+
+export SLURM_CPU_BIND="cores"
+
+#removing the old port forwading
+if [ -e port_forwarding_command ]
+then
+  rm port_forwarding_command
+fi
+
+#getting the node name and port
+SERVER="`hostname`"
+PORT_JU=$(($RANDOM + 10000 )) # random number greaten than 10000
+
+echo $SERVER
+echo $PORT_JU
+
+echo "ssh -L localhost:8888:${SERVER}:${PORT_JU} ${USER}@perlmutter-p1.nersc.gov" > port_forwarding_command
+echo "ssh -L localhost:8888:${SERVER}:${PORT_JU} ${USER}@perlmutter-p1.nersc.gov"
+#echo "ssh -L localhost:${PORT_JU}:${SERVER}:${PORT_JU} ${USER}@perlmutter-p1.nersc.gov" > port_forwarding_command
+#echo "ssh -L localhost:${PORT_JU}:${SERVER}:${PORT_JU} ${USER}@perlmutter-p1.nersc.gov"
+
+echo "load module-environment"
+module load  cudnn/8.7.0  nccl/2.15.5-ofi  evp-patch
+
+echo "execute jupyter"
+source ~/.bashrc
+#conda activate genai
+#cd $SCRATCH/ddl-projects #Root/Working directory for the jupyter notebook server
+cd $SCRATCH  #Root/Working directory for the jupyter notebook server
+shifter --module=gpu jupyter lab --no-browser --ip=0.0.0.0 --port=${PORT_JU} --NotebookApp.token=${USER}
+#bash -c "jupyter lab --ip=0.0.0.0 --port=${PORT_JU} --NotebookApp.token='${USER}'"
+echo "end of the job"
